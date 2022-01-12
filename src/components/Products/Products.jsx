228609140d2e8d2325f9a316aaa644654af36e81
zillur-rgb <zillurdeu@gmail.com>
2022-01-12 21:18:53 +0100

    Product Page Completed

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -2,23 +2,34 @@ import React, { useState } from "react";
 import fakeData from "../../fakeData";
 import "./Products.css";
 import SingleProduct from "../SingleProduct/SingleProduct";
+import Cart from "../Cart/Cart";
 
 const Products = () => {
   const firstTen = fakeData.slice(0, 10);
   const [products, setProducts] = useState(firstTen);
+  const [cart, setCart] = useState([]);
 
+  const handleAddProducts = (product) => {
+    const newCart = [...cart, product];
+    setCart(newCart);
+    console.log(newCart);
+  };
   return (
     <div className="product-container">
       <div className="product-part">
         {products.map((product) => (
-          <SingleProduct product={product} key={product.key}>
+          <SingleProduct
+            product={product}
+            key={product.key}
+            handleAddProducts={handleAddProducts}
+          >
             {product.name}
           </SingleProduct>
         ))}
       </div>
 
       <div className="cart-part">
-        <h3>This is cart</h3>
+        <Cart cart={cart}></Cart>
       </div>
     </div>
   );
--- src/components/SingleProduct/SingleProduct.jsx
+++ src/components/SingleProduct/SingleProduct.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import "./SingleProduct.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
 
 const SingleProduct = (props) => {
-  console.log(props.product);
+  // console.log(props.product);
   const { img, name, seller, price, stock } = props.product;
   return (
     <div className="product-div">
@@ -12,6 +14,16 @@ const SingleProduct = (props) => {
         <p>by {seller}</p>
         <h4>${price}</h4>
         <p>Only {stock} stock remaining - Order fast</p>
+        <button
+          className="btn-cart"
+          onClick={() => {
+            props.handleAddProducts(props.product);
+          }}
+        >
+          <div>
+            <FontAwesomeIcon icon={faShoppingCart} /> Add to Cart
+          </div>
+        </button>
       </div>
     </div>
   );
