f887e6189f5da2341e707f3ce48c4a0d61d9539e
zillur-rgb <zillurdeu@gmail.com>
2022-07-19 10:38:31 +0200

    Route setup basic

--- src/components/LeftNavbar.vue
+++ src/components/LeftNavbar.vue
@@ -6,9 +6,11 @@
       :key="index"
     >
       <button class="py-1 my-1 blue-darken-4 w-100">
-        <v-icon :icon="icon.icon" />
-        <div class="text-caption text-center">{{ icon.name }}</div>
-        <v-divider></v-divider>
+        <router-link :to="{ name: 'About', params: { link: icon.link } }">
+          <v-icon :icon="icon.icon" />
+          <div class="text-caption text-center">{{ icon.name }}</div>
+          <v-divider></v-divider>
+        </router-link>
       </button>
     </div>
   </div>
@@ -20,66 +22,82 @@ export default {
     const leftNavIcons: {
       name: string
       icon: string
+      link?: string
     }[] = [
       {
         name: 'Dashboard',
         icon: 'mdi:mdi-view-dashboard',
+        link: '123',
       },
       {
         name: 'Accounting',
         icon: 'mdi:mdi-text-box-outline',
+        link: '123',
       },
       {
         name: 'Coudinary',
         icon: 'mdi:mdi-cloud-circle',
+        link: '123',
       },
       {
         name: 'Data Hub',
         icon: 'mdi:mdi-axis-arrow',
+        link: '123',
       },
       {
         name: 'Data Hub Importer',
         icon: 'mdi:mdi-axis-arrow',
+        link: '123',
       },
       {
         name: 'Documents',
         icon: 'mdi:mdi-file-document',
+        link: '123',
       },
       {
         name: 'ECM',
         icon: 'mdi:mdi-animation',
+        link: '123',
       },
       {
         name: 'E-mail',
         icon: 'mdi:mdi-email',
+        link: '123',
       },
       {
         name: 'Flux Workflow',
         icon: 'mdi:mdi-sitemap',
+        link: '123',
       },
       {
         name: 'Freight Carrier',
         icon: 'mdi:mdi-label-variant',
+        link: '123',
       },
       {
         name: 'Fulfillment',
         icon: 'mdi:mdi-check-decagram',
+        link: '123',
       },
       {
         name: 'Omni-channel',
         icon: 'mdi:mdi-star-three-points',
+        link: '123',
       },
       {
         name: 'Document List',
         icon: 'mdi:mdi-text-box-check',
+        link: '123',
       },
       {
         name: 'Settings',
         icon: 'mdi:mdi-cog',
+        link: 'about',
       },
       {
         name: 'payments',
         icon: 'mdi:mdi-credit-card-marker',
+        link: '123',
       },
     ]
     return { leftNavIcons }
@@ -93,5 +111,10 @@ button {
 
 button:hover {
   opacity: 0.7;
+}
+
+a {
+  color: #fff;
+  text-decoration: none;
 }
 </style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,3 +1,4 @@
+import AboutView from '@/views/AboutView.vue'
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
@@ -5,21 +6,21 @@ const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: HomeView,
   },
   {
-    path: '/about',
-    name: 'about',
+    path: '/:link',
+    name: 'About',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+    component: AboutView,
+  },
 ]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
+  routes,
 })
 
 export default router
